9055dfa43b724963ed19a16cc7eaa30fac49de0e
RomeoLiu-99 <57131657+RomeoLiu-99@users.noreply.github.com>
2019-11-10 16:46:19 +0800

    Add files via upload
    change to train.py

--- train.py
+++ train.py
@@ -19,29 +19,29 @@ from augmentation import ToTensor, RandomCrop
 
 class Trainer(object):
 	def __init__(self):
-		self.device = 'cpu'
-		self.epochs = 2
+		self.device = 'cuda'
+		self.epochs = 10
 
 		# Networks
 		self.model = FCN32s(21)
 
 		# Criterion
-		self.criterion = nn.CrossEntropyLoss(ignore_index=0)
+		self.criterion = nn.CrossEntropyLoss(ignore_index=255)
 
 		# Optimizer
-		self.lr = 1000
+		self.lr = 0.1
 		self.optimizer = optim.Adam(self.model.parameters(), lr = self.lr)
 		self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=18, gamma=0.1)
 
 		# Dataset
 		self.train_set = PascalVOC(transforms=transforms.Compose([
-										RandomCrop(200),
+										RandomCrop(170),
 										ToTensor()
 									]))
 
-		self.valid_set = PascalVOC(names_file='./VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt',
+		self.valid_set = PascalVOC(names_file='./VOCdevkit/VOC2007/ImageSets/Segmentation/val.txt',
 									transforms=transforms.Compose([
-											RandomCrop(200),
+											RandomCrop(170),
 											ToTensor()
 									]))
 
@@ -82,33 +82,12 @@ class Trainer(object):
 			save_checkpoints(state, is_best)
 
 		time_ellapsed = time.time() - tt
-		print('Training complete in {time_ellapsed // 60}m {time_ellapsed % 60}s')
-
-
-	def _train(self, epoch):
-
-		for idx, batch in enumerate(self.train_loader):
-
-			image = batch['image'].to(self.device)
-			label = batch['label'].to(self.device)
-
-			self.optimizer.zero_grad()
-
-			out = self.model(image)
-
-
-			loss = self.criterion(out, label)
-			print(f'loss: {loss}')
-
-			loss.backward()
-
-			self.optimizer.step()
-			self.lr_scheduler.step()
+		print(f'Training complete in {time_ellapsed // 60}m {time_ellapsed % 60}s')
 
 	def _valid(self, epoch):
 		valid_loss = MetricTracker()
     
-		valid_acc  = MetricTracker()
+		#valid_acc  = MetricTracker()
 
 		self.model.eval()
 
@@ -122,14 +101,39 @@ class Trainer(object):
 			out  = self.model(image)
 			loss = self.criterion(out, label)
         
-
-			valid_acc.update(dice_coeff(out, label), out.shape[0])
 			valid_loss.update(loss.item(), out.shape[0])
         
-		print(f'Valid Loss: {valid_loss.avg:.4f} Acc: {valid_acc.avg:.4f}')
+		print(f'Valid Loss: {valid_loss.avg:.4f}')
     
 		self.model.train()        
-		return {'loss': valid_loss.avg, 'acc': valid_acc.avg}
+		return {'loss': valid_loss.avg}
+
+	def _train(self, epoch):
+		train_loss = MetricTracker()
+
+		for idx, batch in enumerate(self.train_loader):
+
+			image = batch['image'].to(self.device)
+			label = batch['label'].to(self.device)
+
+			self.optimizer.zero_grad()
+
+			out = self.model(image)
+
+
+			loss = self.criterion(out, label)
+			#print(f'loss: {loss}')
+
+			loss.backward()
+
+			self.optimizer.step()
+			self.lr_scheduler.step()
+			
+			train_loss.update(loss.item(), out.shape[0])
+			print(f'#{idx} -- loss: {train_loss.avg:.4f}')
+		print(f'Training loss: {train_loss.avg:.4f}')
+
+		return {'loss': train_loss.avg}
 
 if __name__ == '__main__':
     trainer = Trainer()
